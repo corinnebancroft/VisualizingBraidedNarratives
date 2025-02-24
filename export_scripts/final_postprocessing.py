3214daabf85095f8f8f146f714f77eef835affb0
daydin <deniznaydin@gmail.com>
2025-02-24 14:45:09 -0800

    finalized versions of scripts

--- export_scripts/final_postprocessing.py
+++ export_scripts/final_postprocessing.py
@@ -13,15 +13,9 @@ df_no_duplicates = df[~df.duplicated(
 
 df_no_duplicates["category"] = pd.Categorical(df_no_duplicates["category"], categories=["Exchange", "Knows", "Knows of"], ordered=True)
 
-
-# df_no_duplicates_with_rel_type_diff_kind = df_no_duplicates.drop_duplicates(subset=['Character 1 <br />(choose)', 'Character 2 <br />(choose)', 'startPage'],keep=False).sort_values(by=['Character 1 <br />(choose)', 'Character 2 <br />(choose)', 'startPage'])
-
 df_no_duplicates = df_no_duplicates.sort_values(by=['Character 1 <br />(choose)', 'Character 2 <br />(choose)', 'startPage', 'category']).drop_duplicates(subset=['Character 1 <br />(choose)', 'Character 2 <br />(choose)', 'startPage'])
-
-# final_df = final_df[['source', 'target', "Character 1 <br />(choose)",
-#                      "Character 2 <br />(choose)", 'type', 'weight', 'startPage', 'category']]
 
 date = datetime.now().strftime("%Y%m%d-%HH%MM")
 df_no_duplicates.to_csv(f'data/pod/Feb212025_exports/TEST_pod_exports_TWICE_processed_final_{date}.csv', index=False)
 
-pass
+
---
+++ export_scripts/text_space_edges.py
@@ -0,0 +1,55 @@
+import itertools
+from datetime import datetime
+
+import pandas as pd
+
+# Read the final data
+
+df_og = pd.read_csv('data/pod/Feb212025_exports/TEST_pod_exports_TWICE_processed_final_20250221-10H35M.csv')
+
+char1_tuples = df_og.groupby(by='startPage')['source'].apply(list)
+char2_tuples = df_og.groupby(by='startPage')['target'].apply(list)
+
+df = pd.concat([char1_tuples, char2_tuples], axis=1)
+
+s = df['source'] + df['target']
+
+# Create the dictionary using list comprehension
+result = {i: list(set(value)) for i, value in s.items()}
+
+# final_df = pd.DataFrame(columns=['source', 'target', 'startPage'])
+
+i = 0
+rows_list = []
+# now append each permutation of a tuple as an entry on a given page
+for page_num, characters in result.items():
+    # Generate all unique tuple permutations
+    permutations = set(itertools.combinations(characters, 2))
+    # Convert to a list if you prefer to work with a list of tuples
+    permutations = list(permutations)
+
+    for permutation in permutations:
+        # Extracting character names from df_og for the source and target
+        char1_name_series = df_og.loc[df_og['source'] == permutation[0], 'Character 1 <br />(choose)']
+
+        if not char1_name_series.empty:
+            char1_name = char1_name_series.iloc[0]
+        else:
+            char1_name = df_og.loc[df_og['target'] == permutation[0], 'Character 2 <br />(choose)'].iloc[0]
+
+        char2_name_series = df_og.loc[df_og['target'] == permutation[1], 'Character 2 <br />(choose)']
+
+        if not char2_name_series.empty:
+            char2_name = char2_name_series.iloc[0]
+        else:
+            char2_name = df_og.loc[df_og['source'] == permutation[1], 'Character 1 <br />(choose)'].iloc[0]
+
+        row_dict = {'source': permutation[0], 'target': permutation[1], 'Char 1 Name': char1_name,
+                    'Char 2 Name': char2_name, 'startPage': page_num, 'type': 'Undirected', 'weight': 1}
+
+        i += 1
+        rows_list.append(row_dict)
+final_df = pd.DataFrame(rows_list)
+
+date = datetime.now().strftime("%Y%m%d-%HH%MM")
+final_df.to_csv(f'data/pod/Feb212025_exports/TEST_pod_exports_text_space_edges_{date}.csv', index=False)
