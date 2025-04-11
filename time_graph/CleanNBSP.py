10fc5cc81afc0d96f02bae78555bb762ac053595
corinnebancroft <corinnebancroft@gmail.com>
2025-04-10 17:25:43 -0700

    I'm experimenting with plotly

--- time_graph/CleanNBSP.py
+++ time_graph/CleanNBSP.py
@@ -1,8 +1,8 @@
 
-with open('ExperimentPieChart3.py', 'r', encoding='utf-8') as file:
+with open('Empty.py', 'r', encoding='utf-8') as file:
     content = file.read()
 
 content = content.replace('\u00A0', ' ')
 
-with open('ExperimentPieChart4.py', 'w', encoding='utf-8') as file:
+with open('PlotlyPie.py', 'w', encoding='utf-8') as file:
     file.write(content)
---
+++ time_graph/Empty.py
@@ -0,0 +1,44 @@
+
+import plotly.graph_objects as go
+
+# Data for scatter plot
+scatter_x = [1, 2, 3, 4, 5]
+scatter_y = [10, 11, 12, 13, 14]
+
+# Data for pie charts
+pie_data = [
+    [10, 20, 30, 40],
+    [20, 30, 10, 40],
+    [30, 10, 20, 40],
+    [40, 20, 30, 10],
+    [10, 40, 20, 30]
+]
+
+# Create scatter plot
+fig = go.Figure()
+
+# Add scatter plot points
+fig.add_trace(go.Scatter(
+    x=scatter_x,
+    y=scatter_y,
+    mode='markers',
+    marker=dict(size=20, symbol='circle', opacity=0)  # Invisible markers to position the pies
+))
+
+# Add pie charts as annotations
+for i in range(len(scatter_x)):
+    fig.add_trace(go.Pie(
+        values=pie_data[i],
+        labels=['A', 'B', 'C', 'D'],
+        domain=dict(x=[scatter_x[i] / 6 - 0.05, scatter_x[i] / 6 + 0.05], y=[scatter_y[i] / 15 - 0.05, scatter_y[i] / 15 + 0.05]),
+        hole=0.3,
+        showlegend=False
+    ))
+
+# Update layout to remove grid and axes
+fig.update_layout(
+    xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+    yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
+)
+
+fig.show()
