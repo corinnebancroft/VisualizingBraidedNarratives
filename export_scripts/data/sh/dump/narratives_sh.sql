9453f2f999916659648f8c7edc63fb9d5991da7f
Corinne <corinnebancroft@gmail.com>
2026-01-24 05:56:26 -0500

    I getDBS and generated CSVs

--- export_scripts/data/sh/dump/narratives_sh.sql
+++ export_scripts/data/sh/dump/narratives_sh.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.43, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.44, for Linux (x86_64)
 --
 -- Host: persimmon.hcmc.uvic.ca    Database: narratives_sh
 -- ------------------------------------------------------
--- Server version	8.0.43
+-- Server version	8.0.44
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -248,4 +248,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-21  1:30:05
+-- Dump completed on 2026-01-24  1:30:07
